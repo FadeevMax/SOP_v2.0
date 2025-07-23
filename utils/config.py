cf18877a32daec0e408c80ead156cd1e21ccb49b
FadeevMax <90524904+FadeevMax@users.noreply.github.com>
2025-07-23 16:01:34 +0200

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -11,9 +11,10 @@ PDF_CACHE_PATH = os.path.join(CACHE_DIR, "cached_sop.pdf")
 DOCX_LOCAL_PATH = os.path.join(CACHE_DIR, "sop.docx")
 GDOC_STATE_PATH = os.path.join(CACHE_DIR, "gdoc_state.json")
 ENRICHED_CHUNKS_PATH = os.path.join(CACHE_DIR, "enriched_chunks.json")
+IMAGE_MAP_PATH = os.path.join(CACHE_DIR, "image_map.json")
 
 # === GitHub ===
-GITHUB_REPO = "FadeevMax/SOP_v2.0"
+GITHUB_REPO = "FadeevMax/SOP_sales_chatbot"
 GITHUB_PDF_NAME = "Live_GTI_SOP.pdf"
 GITHUB_TOKEN = st.secrets["GitHub_API"]
 
